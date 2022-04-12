fc73554b278d9b8d0552ea29394da8a4e1ac6f85
Guiml <guilherme@gmail.com>
2022-04-11 22:07:42 -0500

    Day 14 second iteration

---
+++ Project_day14.py
@@ -0,0 +1,79 @@
+from Project_day14_data import dfInstagram
+from art import logoHL, vs
+import random
+from replit import clear
+
+
+
+def inequalizer(a,b):
+    ''''
+    Function to make sure 2 records are not the same
+    '''
+    while a == b:
+        b = random.randint(1,49)        
+    return(a, b)
+
+def randomizer():
+    ''''
+    Function to pick two records
+    '''
+    randA, randB = inequalizer(random.randint(1,49), random.randint(1,49))
+    recA = dfInstagram[randA]
+    recB = dfInstagram[randB]
+    return(recA, recB, randA, randB)
+
+
+
+def game():
+    ''''
+    Main game function
+    '''
+    count_right = 0
+    end_game = False
+    while end_game == False:
+        clear()
+        print(logoHL)
+        
+        recA, recB, randA, randB = randomizer()
+        if count_right > 0:
+            print(f"You are right! Current score: {count_right}")
+            if right == "A":
+                randA, randB = inequalizer(previous, randB)
+                recA, recB = dfInstagram[randA], dfInstagram[randB]
+            elif right == "B":
+                randA, randB = inequalizer(previous, randA)
+                recA, recB = dfInstagram[randA], dfInstagram[randB]
+        
+            
+
+        print(f"Compare A: {recA['name']}, {recA['description']}, from {recA['country']} ")
+        print(vs)
+        print(f"Against B: {recB['name']}, {recB['description']}, from {recB['country']} ")
+        #print(recA['follower_count'],recB['follower_count'])
+        guess = input(f"Who has more followers? Type 'A' or 'B': ")
+        
+        
+        if guess == 'A':
+            if int(recA['follower_count']) > int(recB['follower_count']):
+                count_right += 1
+                previous = randA
+                right = "A"
+            else: 
+                end_game = True
+        if guess == 'B':
+            if int(recB['follower_count']) > int(recA['follower_count']):
+                count_right += 1
+                previous = randB
+                right = "B"
+
+            else: 
+                end_game = True
+
+    if end_game == True:
+        clear()
+        print(logoHL)
+        print(f"Sorry, that's wrong! Final score: {count_right}")
+
+game()  
+
+
